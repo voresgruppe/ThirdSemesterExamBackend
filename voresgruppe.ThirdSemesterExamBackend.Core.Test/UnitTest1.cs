199bbac55dcf08a3c8f64c05a7631a4b750eec42
EmilseBilse <emil875b@easv365.dk>
2021-11-22 10:44:17 +0100

    test projects

---
+++ voresgruppe.ThirdSemesterExamBackend.Core.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace voresgruppe.ThirdSemesterExamBackend.Core.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
