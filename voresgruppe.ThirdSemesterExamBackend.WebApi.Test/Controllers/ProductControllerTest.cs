8e57052e9a73287790f79a468934c77014019dd2
ceci6182 <ceci6182@easv365.dk>
2021-11-23 22:24:30 +0100

    Create ProductControllerTest.cs

---
+++ voresgruppe.ThirdSemesterExamBackend.WebApi.Test/Controllers/ProductControllerTest.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using voresgruppe.ThirdSemesterExamBackend.WebApi.Controllers;
+using Xunit;
+
+namespace voresgruppe.ThirdSemesterExamBackend.WebApi.Test.Controllers
+{
+    public class ProductControllerTest
+    {
+        private readonly HairStyleController _controller;
+
+        public ProductControllerTest()
+        {
+            _controller = new HairStyleController();
+        }
+
+        [Fact]
+        public void HairstyleController_IsOfTypeControllerBase()
+        {
+           Assert.IsAssignableFrom<ControllerBase>(_controller);
+        }
+
+        [Fact]
+        public void HairstyleController_UsesApiControllerAttribute()
+        {
+            var typeInfo = typeof(HairStyleController).GetTypeInfo();
+            var attribute = typeInfo.GetCustomAttributes()
+                .FirstOrDefault(a=> a.GetType()
+                    .Name.Equals("ApiControllerAttribute"));
+            Assert.NotNull(attribute);
+        }
+
+        [Fact]
+        public void HairStyleController_UsesRouteAttribute_WithParamApiControllerNameRoute()
+        {
+            var typeInfo = typeof(HairStyleController).GetTypeInfo();
+            var attribute = typeInfo.GetCustomAttributes()
+                .FirstOrDefault(a=> a.GetType()
+                    .Name.Equals("RouteAttribute"));
+            Assert.NotNull(attribute);
+            var routeAttr = attribute as RouteAttribute;
+            Assert.Equal("api/[controller]", routeAttr.Template);
+        }
+    }
+}
