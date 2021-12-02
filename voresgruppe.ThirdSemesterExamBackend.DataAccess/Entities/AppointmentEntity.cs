fef498f9a3f6d333bd0c65e3d46a793c845cb47f
EmilseBilse <emil875b@easv365.dk>
2021-12-02 15:28:05 +0100

    jeg tror at jeg har linket customer og appointment sammen i databsen
    jeg skal smutte nu vi håber at det dur

--- voresgruppe.ThirdSemesterExamBackend.DataAccess/Entities/AppointmentEntity.cs
+++ voresgruppe.ThirdSemesterExamBackend.DataAccess/Entities/AppointmentEntity.cs
@@ -1,7 +1,12 @@
-﻿namespace voresgruppe.ThirdSemesterExamBackend.DataAccess.Entities
+﻿using System;
+
+namespace voresgruppe.ThirdSemesterExamBackend.DataAccess.Entities
 {
     public class AppointmentEntity
     {
-        
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public CustomerEntity Customer { get; set; }
+        public DateTime AppointmentTime { get; set; }
     }
 }
