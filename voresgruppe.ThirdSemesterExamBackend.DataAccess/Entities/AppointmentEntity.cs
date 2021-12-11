4c7eb80866631ff8f606d8e0253adc1f016881f3
lazynormz <skupi1234@gmail.com>
2021-12-11 21:47:59 +0100

    Cleanup

--- voresgruppe.ThirdSemesterExamBackend.DataAccess/Entities/AppointmentEntity.cs
+++ voresgruppe.ThirdSemesterExamBackend.DataAccess/Entities/AppointmentEntity.cs
@@ -6,14 +6,9 @@ namespace voresgruppe.ThirdSemesterExamBackend.DataAccess.Entities
     {
         public int Id { get; set; }
         public int CustomerId { get; set; }
-        
-        //public CustomerEntity Customer { get; set; }
         public DateTime AppointmentTime { get; set; }
-        
         public int EmployeeId { get; set; }
-        
         public int HairstyleStarterId { get; set; }
-        
         public int HairstyleEndId { get; set; }
     }
 }
